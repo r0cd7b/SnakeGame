14c530ae1a5285b813bb3b3d1e8f986f8858d786
r0cd7b <tkdrbs1928@naver.com>
2020-11-13 01:22:07 +0900

    This is the final version.

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Level";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.InterceptArrowKeys = false;
+            this.numericUpDown1.Location = new System.Drawing.Point(57, 10);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(105, 21);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(167, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(10, 34);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.progressBar1.Maximum = 1;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(234, 18);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 58);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(234, 234);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(255, 300);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Snake";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Snake/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        int bitmapWidth, bitmapHeight, adjustment, rudder, speed;
+        Bitmap bitmap;
+        Graphics graphics;
+        const int rectangleWidth = 10, rectangleHeight = 10;
+        LinkedList<Rectangle> snake;
+        Size[] direction;
+        Random random;
+        Rectangle food;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bitmapWidth = pictureBox1.ClientSize.Width;
+            bitmapHeight = pictureBox1.ClientSize.Height;
+            bitmap = new Bitmap(bitmapWidth, bitmapHeight);
+            graphics = Graphics.FromImage(bitmap);
+            snake = new LinkedList<Rectangle>();
+            direction = new Size[4] { new Size(0, -10), new Size(10, 0), new Size(0, 10), new Size(-10, 0) };
+            random = new Random();
+            food = new Rectangle(0, 0, rectangleWidth, rectangleHeight);
+            speed = timer1.Interval;
+        }
+
+        void FoodAppear()
+        {
+            do
+            {
+                food.X = random.Next(bitmapWidth / rectangleWidth) * rectangleWidth;
+                food.Y = random.Next(bitmapHeight / rectangleHeight) * rectangleHeight;
+            }
+            while (snake.Contains(food));
+            RenewPicture();
+        }
+
+        void RenewPicture()
+        {
+            graphics.Clear(Color.Transparent);
+            foreach (Rectangle body in snake)
+                graphics.FillRectangle(Brushes.Black, body);
+            graphics.FillRectangle(Brushes.Red, food);
+            pictureBox1.Image = bitmap;
+        }
+
+        void HeadImpact()
+        {
+            timer1.Stop();
+            graphics.DrawString("You Died", new Font(Font.FontFamily, 20, FontStyle.Bold), Brushes.Gray, 50, 90);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            progressBar1.Value = 0;
+            rudder = 0;
+            snake.Clear();
+            snake.AddFirst(new Rectangle(bitmapWidth / rectangleWidth / 2 * rectangleWidth, bitmapHeight / rectangleHeight / 2 * rectangleHeight, rectangleWidth, rectangleHeight));
+            FoodAppear();
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Rectangle tail = snake.Last.Value;
+            adjustment = rudder;
+            tail.Location = Point.Add(snake.First.Value.Location, direction[adjustment]);
+            snake.AddFirst(tail);
+
+            Rectangle head = snake.First.Value;
+            if (head.Location == food.Location)
+            {
+                if (++progressBar1.Value == progressBar1.Maximum)
+                {
+                    decimal level = numericUpDown1.Value + 1;
+                    if (level > numericUpDown1.Maximum)
+                        progressBar1.Value = 0;
+                    else
+                        numericUpDown1.Value = level;
+                }
+                FoodAppear();
+                return;
+            }
+
+            snake.RemoveLast();
+            RenewPicture();
+            int headX = head.X, headY = head.Y;
+            if (headX < 0 || headX >= bitmapWidth || headY < 0 || headY >= bitmapHeight)
+            {
+                HeadImpact();
+                return;
+            }
+            if (snake.Count > 4)
+            {
+                LinkedListNode<Rectangle> body = snake.First.Next.Next.Next.Next;
+                while (body != null)
+                {
+                    if (head.Location == body.Value.Location)
+                    {
+                        HeadImpact();
+                        return;
+                    }
+                    body = body.Next;
+                }
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            int level = (int)numericUpDown1.Value;
+            progressBar1.Maximum = level;
+            timer1.Interval = speed - speed / (int)numericUpDown1.Maximum * (level - 1);
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.Left:
+                    rudder = (adjustment + 3) % 4;
+                    break;
+                case Keys.Right:
+                    rudder = (adjustment + 1) % 4;
+                    break;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
